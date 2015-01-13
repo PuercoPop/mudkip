36fbc13ca72465503d4733053e7d640b469f0f3d
Javier Olaechea <pirata@gmail.com>
2015-01-13 13:30:18 -0500

    Router a la wookie for the time being

--- src/core/router.lisp
+++ src/core/router.lisp
@@ -3,9 +3,9 @@
   (:export
    #:retrieve-document
    #:list-all-urls
-   #:add-route
-   #:router))
-(in-package :mudkip/core/router)
+   #:add-route))
+
+(in-package #:mudkip/core/router)
 
 (defgeneric retrieve-document (router url)
   (:documentation "Retrieve the document to render for the current url."))
@@ -15,28 +15,3 @@
 
 (defgeneric add-route (router url-selector document-selector)
   (:documentation "Add a new route to the router."))
-
-(defclass router ()
-  ((routes :initarg :db :reader routes :documentation "A hash table where the
-  key is a url/regexp and the value a document optima pattern.")))
-
-;; /posts/<title:str>/
-(defmethod retrieve-document ((router router) (url string))
-  (with-hash-table-iterator (next (routes router))
-    (loop
-       (multiple-value-bind (more? key value) (next)
-         (unless more? (return))
-         )
-       )))
-
-;; TODO: around-method to verify the query returns only one document.
-;; (add-route (router site) (make-instance 'url-selector
-;;                                         :template
-;;                                         "/posts/<str:name>/" (document :slug name)))
-(defmethod add-route ((router router) url-selector query)
-  )
-
-
-
-;; (make-url-selector "/posts/<str:name>/")
-;; returns nil or a plist with the environment. Should be easy to add a let around. Should I use (values match env) instead?
---
+++ src/router.lisp
@@ -0,0 +1,27 @@
+(uiop:define-package #:mudkip/router
+  (:use :cl)
+  (:use-reexport :mudkip/core/router)
+  (:import-from :ppcre #:create-scanner
+                       #:scan))
+(in-package :mudkip/router)
+
+(defclass router ()
+  ((routes :initarg :db :initform (make-hash-table) :reader routes
+           :documentation "A hash table mapping a regexp with a
+           document query pattern.")))
+
+;; /posts/<title:str>/
+(defmethod retrieve-document ((router router) (url string))
+  (loop :for key :being :the hash-keys :of (routes router)
+        :for value :being :the hash-values :of (routes router)
+        :when (scran key url)
+          (return value)))
+
+;; TODO: around-method to verify the query returns only one document.
+(defmethod add-route ((router router) url-selector query)
+  (setf (gethash (create-scanner url-selector) (routes router)) query))
+
+
+
+;; (make-url-selector "/posts/<str:name>/")
+;; returns nil or a plist with the environment. Should be easy to add a let around. Should I use (values match env) instead?
